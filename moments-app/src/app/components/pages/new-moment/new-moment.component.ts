254b9a0a267df9c9e7eccff4fc4d5b61ea4f1c47
pedromlsr <pedrodesarodrigues@gmail.com>
2023-01-16 23:53:28 -0300

    página de formulário

--- moments-app/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments-app/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-new-moment',
-  templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+    selector: 'app-new-moment',
+    templateUrl: './new-moment.component.html',
+    styleUrls: ['./new-moment.component.css']
 })
 export class NewMomentComponent implements OnInit {
+    btnText: string = 'Compartilhar!'
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
