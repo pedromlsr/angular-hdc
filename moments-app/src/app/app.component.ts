ff8637f53ca75ee71efdf34c9aba45327c40e055
pedromlsr <pedrodesarodrigues@gmail.com>
2023-01-16 22:44:12 -0300

    setup inicial do projeto

--- moments-app/src/app/app.component.ts
+++ moments-app/src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    userName = 'pedromlsr'
-
-    userData = {
-        email: 'pedromlsr@gmail.com',
-        role: 'Admin'
-    }
-
-    title = 'moments-app'
+  title = 'moments-app';
 }
